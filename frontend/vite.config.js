c6f9ec062b8fdecbfa32237483886981578a889a
Goodmann <bensonisonline@gmail.com>
2024-07-12 18:14:21 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 5000,
+    host: true, 
     proxy: {
       '/api': {
         target: 'https://waitlist-lbuq.onrender.com',
